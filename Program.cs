ebbf65e69b0e9c90ed9859b9b2c6f51d6a7c3b32
Nurtleu <nur71701@gmail.com>
2017-07-18 14:30:45 +0600

    c# homework1

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Имю и Фамилию:");
+            string name = Console.ReadLine();
+            //Console.WriteLine("Введите Фамилию:");
+            string fname = Console.ReadLine();
+            Console.WriteLine("Введитe возраст:");
+            string age = Console.ReadLine();
+
+
+
+            Console.WriteLine("Ваше Имя и  Фамилия:");
+            Console.WriteLine(name);
+            Console.WriteLine(" ");
+            Console.WriteLine(fname);
+            Console.WriteLine("Ваш возраст:");
+            Console.WriteLine(age);
+
+
+
+            Console.WriteLine("Введите 1-Число:");
+            String num1String = Console.ReadLine();
+            int num1Int = Int32.Parse(num1String);
+
+            Console.WriteLine("Введите 2-Число:");
+            String num2String = Console.ReadLine();
+            int num2Int = Int32.Parse(num2String);
+
+            Console.Write(num1Int);
+            num1Int = num1Int * num2Int;
+            Console.Write(" * ");
+            Console.WriteLine(num2Int);
+            Console.Write(" равно: ");
+            Console.WriteLine(num1Int);
+
+            Console.ReadLine();    
+        }
+    }
+}
